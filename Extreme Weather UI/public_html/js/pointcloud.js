ff0254994deb625c4d3afb110528b59d401facc3
Mario-S <spindizzy@outlook.com>
2015-10-24 12:11:35 +0200

    added converter function

--- Extreme Weather UI/public_html/js/pointcloud.js
+++ Extreme Weather UI/public_html/js/pointcloud.js
@@ -1,25 +1,33 @@
-var createPointCloud = function(scene) {
-  // add points with 15 units distance from center
-
-  var group = new THREE.Group();
-
-  var r = 15;
-  for(var i=0; i<200; i++) {
-    var geometry = new THREE.SphereGeometry( 0.1, 10, 10 );
-    var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-
-    var a = Math.random() * Math.PI;
-    var b = Math.random() * Math.PI * 2;
-
-    var x = r * Math.sin(a) * Math.cos(b);
-    var y = r * Math.sin(a) * Math.sin(b);
-    var z = r * Math.cos(a);
-
-    geometry.translate(x, y, z);
+var createPointCloud = function (scene) {
     
-    var sphere = new THREE.Mesh( geometry, material );
-    group.add( sphere );
-  }
-  group.name = 'points';
-  scene.add(group);
-}
+
+    var convert = function (coordinate) {
+        var r = 15; //radius of our earth model
+
+        var a = coordinate.a;
+        var b = coordinate.b;
+
+        var x = r * Math.sin(a) * Math.cos(b);
+        var y = r * Math.sin(a) * Math.sin(b);
+        var z = r * Math.cos(a);
+        return {'x': x, 'y': y, 'z': z};
+    };
+
+    var group = new THREE.Group();
+
+    for (var i = 0; i < 200; i++) {
+        var geometry = new THREE.SphereGeometry(0.1, 10, 10);
+        var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+
+        var a = Math.random() * Math.PI;
+        var b = Math.random() * Math.PI * 2;
+
+        var coor = convert({'x': a, 'y': b});
+        geometry.translate(coor.x, coor.y, coor.z);
+
+        var sphere = new THREE.Mesh(geometry, material);
+        group.add(sphere);
+    }
+    group.name = 'points';
+    scene.add(group);
+};
